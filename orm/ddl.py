91d5120b4462ce29568accb27908d5197405a5da
Szymon <149382092+Szaj111@users.noreply.github.com>
2024-01-26 19:39:54 +0100

    wywalilo_wszystko :)

--- orm/ddl.py
+++ orm/ddl.py
@@ -8,7 +8,7 @@ class User(Base):
     id= sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.Sequence("user_id"),autoincrement=True, primary_key=True)
     nick = sqlalchemy.Column(sqlalchemy.String, unique=True)
     name = sqlalchemy.Column(sqlalchemy.String)
-    subscription = sqlalchemy.Column(sqlalchemy.String)
+    subscription_id = sqlalchemy.Column(sqlalchemy.Integer)
     city = sqlalchemy.Column(sqlalchemy.String)
 
 
@@ -17,18 +17,17 @@ class Movie(Base):
     id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.Sequence("movie2323"), autoincrement=True,primary_key=True)
     movie_name = sqlalchemy.Column(sqlalchemy.String)
     category = sqlalchemy.Column(sqlalchemy.String)
-
+    subscription_id = sqlalchemy.Column(sqlalchemy.String)
 class Movies_watched(Base):
     __tablename__ = "movies_watched2323"
     id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.Sequence("movies_watched"), autoincrement=True,primary_key=True)
     movie_id = sqlalchemy.Column(sqlalchemy.Integer)
     user_id = sqlalchemy.Column(sqlalchemy.Integer)
-    category_id = sqlalchemy.Column(sqlalchemy.String)
+
 class Subscription(Base):
     __tablename__ = "subscriptions2323"
     id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.Sequence("subscription"), autoincrement=True, primary_key=True)
     subscription = sqlalchemy.Column(sqlalchemy.String)
     movie_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("movies.id"))
-
     movie = relationship("Movie")
 
